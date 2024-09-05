a557e076dac87f4027a62e4df434b48204a3b1f9
kamlesh sahani <alex20150001969@gmail.com>
2024-09-05 19:24:48 +0530

    protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,11 @@ const  {auth} =  NextAuth(authConfig);
   export const middleware = async (req:NextRequest)=>{
     const session:any  = await auth();
     const isLogin = !!session || !!session?.user || !!session?.user.email;
-    const protectedRoute = req.nextUrl.pathname==="/campaign" || req.nextUrl.pathname==="/profile";
+    const protectedRoute = req.nextUrl.pathname==="/campaign" || req.nextUrl.pathname==="/profile" || req.nextUrl.pathname==="/api/me" || req.nextUrl.pathname==="/api/send-mail";
     const withoutLoginRoute = req.nextUrl.pathname === "/login" || req.nextUrl.pathname==="/register"
     if(isLogin && withoutLoginRoute){
         return  NextResponse.redirect(new URL('/', req.url))
     }
-
     if(protectedRoute && !isLogin){
         return NextResponse.redirect(new URL("/login"))
     }
